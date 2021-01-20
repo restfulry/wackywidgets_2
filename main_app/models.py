6d2e6247bcbed302bf528d2c10dadc9510227a6c
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-19 21:44:11 -0500

    create Widget model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Widget(models.Model):
+    description = models.CharField(max_length=150)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.description
