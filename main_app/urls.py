554827daa8ab06d0a4be5d21eb3798a09b6748c9
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-01-19 22:36:37 -0500

    add delete function

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home_index, name='home_index'),
     path('add/', views.add_widget, name='add_widget'),
+    path('widget/<int:widget_id>/delete/',
+         views.delete_widget, name='delete_widget'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -16,3 +16,8 @@ def add_widget(request):
         new_widget = form.save(commit=False)
         new_widget.save()
     return redirect('home_index')
+
+
+def delete_widget(request, widget_id):
+    Widget.objects.get(id=widget_id).delete()
+    return redirect('home_index')
